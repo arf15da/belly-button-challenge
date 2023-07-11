dc9162e22c1cf5ac801da9e97646ebb7cea6c02f
arf15da <lilboila27@gmail.com>
2023-07-10 21:48:33 -0500

    updating repo

--- Starter Code/static/js/app.js
+++ Starter Code/static/js/app.js
@@ -1,15 +1,16 @@
 const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
 
 //pull the demographic info pair from metadata base on the sample on init
-function demo_data(sample) {
+function demograph_data(sample) {
     d3.json(url).then((data) => {
         let metadata = data.metadata;
-        let resultarray = metadata.filter(x => x.id == sample) //filter for the argument sample to match with id
+        let resultarray = metadata.filter(x => x.id == sample) 
+        //filter for the argument sample to match with id
         let result = resultarray[0]
         let table = d3.select("#sample-metadata")
-        table.html("") //clear the data on the table
-        //for (key in result) {
-        //    table.append("h6").text(`${key}: ${result[key]}`)
+        table.html("") 
+        //clear the data on the table
+        //for (key in result) {table.append("h6").text(`${key}: ${result[key]}`)
         //}
         Object.entries(result).forEach(([key, value]) => {
             table.append("h6").text(`${key}: ${value}`)
@@ -55,10 +56,15 @@ function init() {
 
         })
         let firstSample = names[0]
-        demo_data(firstSample)
+        demograph_data(firstSample)
         plot_data(firstSample)
-        plot_gauge(firstSample)
 
     })
 }
 init()
+
+// create a function to update base on optionChanged event
+function optionChanged(newSample) {
+    demograph_data(newSample)
+    plot_data(newSample)
+}
